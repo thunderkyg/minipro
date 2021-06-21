e6feb5e507c13235ae19923621fa608f15b9d1f0
thunderkyg <thunderkyg@gmail.com>
2021-06-21 14:50:00 +0900

    minipro (2021/06/21)

---
+++ src/com/javaex/ex01/PhoneBook.java
@@ -0,0 +1,62 @@
+package com.javaex.ex01;
+
+public class PhoneBook {
+
+	// Field
+	String name;
+	String hp;
+	String company;
+
+	// Constructor
+
+	public PhoneBook() {
+
+	}
+
+	public PhoneBook(String name, String hp, String company) {
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+
+
+	// Method - gs
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getHp() {
+		return hp;
+	}
+	
+	public String getCompany() {
+		return company;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	
+	// Method - Ordinary
+	
+	@Override
+	public String toString() {
+		return "PhoneBook [name=" + name + ", hp=" + hp + ", company=" + company + "]";
+	}
+	
+	public String showInfo() {
+		return name + "	" + hp + "	" + company;
+	}
+	
+	
+}
---
+++ src/com/javaex/ex01/PhoneBookApp.java
@@ -0,0 +1,112 @@
+package com.javaex.ex01;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class PhoneBookApp {
+
+	public static void main(String[] args) throws IOException {
+
+		//스캐너
+		Scanner input = new Scanner(System.in);
+		
+		//시작
+		System.out.println("****************************************");
+		System.out.println("*       전화번호 관리 프로그램         *");
+		System.out.println("****************************************");
+		
+		// Stream
+		InputStream is = new FileInputStream("C:\\javaStudy\\workspace\\minipro\\미니프로젝트\\PhoneDB.txt");
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		
+		//ListArray
+		List<PhoneBook> pArray = new ArrayList<PhoneBook>();
+		
+		// text 파일 읽기 반복문
+		String data;
+		while (true) {
+			data = br.readLine();
+			if (data == null) {
+				break;
+			}
+			String[] phoneInfo = data.split(",");
+			String name = phoneInfo[0];
+			String hp = phoneInfo[1];
+			String company = phoneInfo[2];
+			pArray.add(new PhoneBook(name, hp, company));
+		}
+		
+		//반복문
+		while(true) {
+			System.out.println("1.리스트  2.등록  3.삭제  4.검색  5.종료");
+			System.out.println("----------------------------------------");
+			System.out.print(">메뉴번호: ");
+			String num = input.nextLine();
+			int i = 1;
+			//리스트
+			if("1".equals(num)) {
+				System.out.println("<1.리스트>");
+				for(PhoneBook list : pArray) {
+					System.out.println(i + ". " + list.showInfo());
+					i++;
+				}
+				System.out.println();
+			//등록
+			} else if("2".equals(num)) {
+				System.out.println("<2.등록>");
+				System.out.print(">이름: ");
+				String name = input.nextLine();
+				System.out.print(">휴대전화: ");
+				String hp = input.nextLine();
+				System.out.print(">회사전화: ");
+				String company = input.nextLine();
+				pArray.add(new PhoneBook(name, hp, company));
+				System.out.println("[등록되었습니다.]");
+				System.out.println();
+			//삭제
+			} else if("3".equals(num)) {
+				System.out.println("<3. 삭제>");
+				System.out.print(">번호: ");
+				int delete = input.nextInt();
+				delete--;
+				pArray.remove(delete);
+				System.out.println("[삭제되었습니다.]");
+				System.out.println();
+			//검색
+			} else if("4".equals(num)) {
+				System.out.println("<4.검색>");
+				System.out.print(">이름: ");
+				String search = input.nextLine();
+				for(PhoneBook list : pArray) {
+					if(list.getName().contains(search))
+					System.out.println(i + ". " + list.showInfo());
+					i++;
+				}
+			//종료
+			} else if("5".equals(num)) {
+				break;
+			//잘못된번호
+			} else {
+				System.out.println("[다시 입력해주세요]");
+				System.out.println();
+			}
+			
+		}
+		System.out.println("****************************************");
+		System.out.println("*             감사합니다               *");
+		System.out.println("****************************************");
+
+		input.close();
+		br.close();
+		
+	}
+
+}
